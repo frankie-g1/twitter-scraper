109a10d2bb81458f9675c2e391eed4d1940386e9
andrewgalvin <47649631+andrewgalvin@users.noreply.github.com>
2021-05-26 11:08:34 -0400

    Rewrite init

--- tweet.py
+++ tweet.py
@@ -1,8 +1,13 @@
 class Tweet:
 
-  def __init__(self, timestamp, profile, content, likes, retweets):
+  def __init__(self, timestamp, tweet_id, tweet_url, username, user_id, replies, retweets, likes, hashtags, urls, photos):
     self.timestamp = timestamp
-    self.profile = profile
-    self.content = content
-    self.likes = likes
+    self.tweet_id = tweet_id
+    self.tweet_url = tweet_url
+    self.username = username
+    self.user_id = user_id
+    self.replies = replies
     self.retweets = retweets
+    self.likes = likes
+    self.hashtags = hashtags
+    self.urls = urls
