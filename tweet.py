7e7de90c2ce74d9cc5666cbc3b6614b11fdceadd
andrewgalvin <47649631+andrewgalvin@users.noreply.github.com>
2021-05-26 11:04:57 -0400

    Initial commit

---
+++ tweet.py
@@ -0,0 +1,8 @@
+class Tweet:
+
+  def __init__(self, timestamp, profile, content, likes, retweets):
+    self.timestamp = timestamp
+    self.profile = profile
+    self.content = content
+    self.likes = likes
+    self.retweets = retweets
