c10125124e71ea2f28d006f510d94c30a2f872ec
Andrew Galvin <andrewggalvin@gmail.com>
2021-05-27 00:30:25 -0400

    Connected firebase and created monitor to constantly monitor

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
 from utils.scraper import get_tweets
+from firebase import firebase
+import threading
+
+
+def post_to_database(query, t):
+    db = firebase.FirebaseApplication("INSERT_URL_HERE", None)
+    db.put(f'/twitter/queries/{query}/{t.timestamp}', name="timestamp", data=t.timestamp)
+    db.put(f'/twitter/queries/{query}/{t.timestamp}', name="tweet_id", data=t.tweet_id)
+    db.put(f'/twitter/queries/{query}/{t.timestamp}', name="tweet_url", data=t.tweet_url)
+    db.put(f'/twitter/queries/{query}/{t.timestamp}', name="username", data=t.username)
+    db.put(f'/twitter/queries/{query}/{t.timestamp}', name="content", data=t.content)
+    db.put(f'/twitter/queries/{query}/{t.timestamp}', name="user_id", data=t.user_id)
+    db.put(f'/twitter/queries/{query}/{t.timestamp}', name="replies", data=t.replies)
+    db.put(f'/twitter/queries/{query}/{t.timestamp}', name="retweets", data=t.retweets)
+    db.put(f'/twitter/queries/{query}/{t.timestamp}', name="likes", data=t.likes)
+    db.put(f'/twitter/queries/{query}/{t.timestamp}', name="hashtags", data=t.hashtags)
+
 
 if __name__ == "__main__":
-    for t in get_tweets(query='btc', count=20):
-        pass
-        # print(t.to_json())
+    tweets = []
+    query = 'btc'
+    while True:
+        new = False
+        threads = []
+        for t in get_tweets(query=query, count=20):
+            if t.tweet_id not in tweets:
+                tweets.append(t.tweet_id)
+                x = threading.Thread(target=post_to_database, args=(query, t))
+                threads.append(x)
+                x.start()
+        for t in threads:
+            t.join()
+        if len(tweets) > 40:
+            del tweets[0:20]
--- utils/scraper.py
+++ utils/scraper.py
@@ -2,12 +2,25 @@ from utils.tweet import Tweet
 import requests
 import json
 import logging
+import random
 
+
+def get_proxies():
+    proxies = []
+    with open('utils/proxies.txt') as f:
+        for data in f.read().splitlines():
+            tmp = data.split(':')
+            proxies.append(tmp)
+    return proxies
 
 def get_tweets(query, count, proxy=None):
+    # Uncomment out if you want to know that you aren't using a proxy -- gets a bit spammy
+    # if proxy is None:
+    #     logging.basicConfig(format='%(asctime)s -> %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+    #     logging.warning('No proxy given, local network will be used -- be careful of getting banned.')
     if proxy is None:
-        logging.basicConfig(format='%(asctime)s -> %(message)s', datefmt='%d-%b-%y %H:%M:%S')
-        logging.warning('No proxy given, local network will be used -- be careful of getting banned.')
+        rand_proxy = random.choice(get_proxies())
+        proxy = { 'https' : f'https://{rand_proxy[2]}:{rand_proxy[3]}@{rand_proxy[0]}:{rand_proxy[1]}' }
     # List of tweets
     tweets = []
     # Default headers
@@ -16,7 +29,7 @@ def get_tweets(query, count, proxy=None):
         "accept": "*/*",
         "accept-language": "en-US,en;q=0.9",
         "authorization": "Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA",
-        "referrer": "https://twitter.com/search?q=btc",
+        "referrer": f"https://twitter.com/search?q={query}",
         "credentials": "include"
     }
     # Open a session
@@ -25,20 +38,18 @@ def get_tweets(query, count, proxy=None):
         # Guest token is REQUIRED to retrieve a response
         try:
             if proxy is not None:
-                headers['x-guest-token'] = \
-                    json.loads(s.post(proxies=proxy, headers=headers,
-                                      url='https://api.twitter.com/1.1/guest/activate.json').content)[
-                        'guest_token']
+                req = s.post(proxies=proxy, headers=headers, url='https://api.twitter.com/1.1/guest/activate.json').content
+                headers['x-guest-token'] = json.loads(req)['guest_token']
             else:
-                headers['x-guest-token'] = \
-                    json.loads(s.post(proxies=proxy, headers=headers,
-                                      url='https://api.twitter.com/1.1/guest/activate.json').content)[
-                        'guest_token']
-        except:
-            raise Exception('Request error trying to retrieve a guest token.')
+                req = s.post(headers=headers,
+                             url='https://api.twitter.com/1.1/guest/activate.json').content
+                headers['x-guest-token'] = json.loads(req)['guest_token']
+        except Exception as e:
+            raise Exception(f'Request error trying to retrieve a guest token.\nTraceback: {e}\nRequest Status Code: {req}')
 
         # Retrieve the last 20 tweets from the given query
         try:
+            headers['cache-control'] = "no-cache"
             if proxy is not None:
                 p = s.get(headers=headers,
                           proxies=proxy,
@@ -47,8 +58,8 @@ def get_tweets(query, count, proxy=None):
                 p = s.get(headers=headers,
                           url=f'https://twitter.com/i/api/2/search/adaptive.json?include_profile_interstitial_type=1&include_blocking=1&include_blocked_by=1&include_followed_by=1&include_want_retweets=1&include_mute_edge=1&include_can_dm=1&include_can_media_tag=1&skip_status=1&cards_platform=Web-12&include_cards=1&include_ext_alt_text=true&include_quote_count=true&include_reply_count=1&tweet_mode=extended&include_entities=true&include_user_entities=true&include_ext_media_color=true&include_ext_media_availability=true&send_error_codes=true&simple_quoted_tweet=true&q={query}&tweet_search_mode=live&count={count}&query_source=&pc=1&spelling_corrections=1&ext=mediaStats%2ChighlightedLabel')
 
-        except:
-            raise Exception('Request error trying to retrieve the last 20 tweets.')
+        except Exception as e:
+            raise Exception(f'Request error trying to retrieve the last 20 tweets.\nTraceback: {e}')
 
         # JSON data of all the tweets
         retrieved_tweets = p.json()['globalObjects']
